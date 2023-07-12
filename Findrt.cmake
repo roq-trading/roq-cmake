855ce5747e4a6ddcb36c04f689f213571fc1bfda
thraneh <thraneh@gmail.com>
2023-07-12 09:07:20 +0200

    chore

--- Findrt.cmake
+++ Findrt.cmake
@@ -1,14 +1,12 @@
 include(FindPackageHandleStandardArgs)
 
-find_library(RT_LIBRARY NAMES "rt")
+find_library(RT_LIBRARY NAMES rt)
 
-find_package_handle_standard_args(rt
-  FOUND_VAR
-    RT_FOUND
-  REQUIRED_VARS
-    RT_LIBRARY
-)
+find_package_handle_standard_args(
+  rt
+  FOUND_VAR RT_FOUND
+  REQUIRED_VARS RT_LIBRARY)
 
-# note! drop prefix (assuming a single filename)
-#   was: set(RT_LIBRARIES "${RT_LIBRARY}")
-set(RT_LIBRARIES "rt")
+# note! drop prefix (assuming a single filename) was: set(RT_LIBRARIES
+# "${RT_LIBRARY}")
+set(RT_LIBRARIES rt)

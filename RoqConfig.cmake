4e7ed1431a0b4d6d09c6b7feda5ac1c474e5ab26
thraneh <thraneh@gmail.com>
2023-09-20 09:36:49 +0200

    more shared roq config

--- RoqConfig.cmake
+++ RoqConfig.cmake
@@ -1,2 +1,35 @@
-# allow benchmark targets per sub-project
+# benchmark targets are per sub-project
+
 set(ALLOW_DUPLICATE_CUSTOM_TARGETS TRUE)
+
+# conda
+
+if(DEFINED ENV{CONDA_PREFIX} AND NOT DEFINED ENV{CONDA_BUILD})
+  message("Setting CMAKE_INSTALL_PREFIX and CMAKE_FIND_ROOT_PATH")
+  set(CMAKE_INSTALL_PREFIX $ENV{CONDA_PREFIX})
+  set(CMAKE_FIND_ROOT_PATH $ENV{CONDA_PREFIX})
+endif()
+
+# rpath
+
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+# c++
+
+if(NOT DEFINED CMAKE_CXX_STANDARD)
+  set(CMAKE_CXX_STANDARD 20)
+endif()
+
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+  message("Setting default visibility to hidden")
+  set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+  set(CMAKE_VISIBILITY_INLINES_HIDDEN 1)
+endif()
+
+# prefer static libraries
+
+set(CMAKE_FIND_LIBRARY_SUFFIXES .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
+
+# filesystem
+
+include(GNUInstallDirs)
